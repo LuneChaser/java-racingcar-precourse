afd3c44295b02f8856eaeafb0203d824fbf9a320
LunChaser <devilking12@naver.com>
2021-10-08 01:04:33 +0900

    test(Test): 단위/인수 테스트 추가
    개발진행시 기능 검증 대상에대한 항목 추가

--- src/test/java/racinggame/ApplicationTest.java
+++ src/test/java/racinggame/ApplicationTest.java
@@ -1,9 +1,16 @@
 package racinggame;
 
 import nextstep.test.NSTest;
+import nextstep.utils.Randoms;
+
+import static org.mockito.Mockito.mockStatic;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 public class ApplicationTest extends NSTest {
     private static final int MOVING_FORWARD = 4;
@@ -31,6 +38,87 @@ public class ApplicationTest extends NSTest {
             verify(ERROR_MESSAGE);
         });
     }
+
+	@Nested
+	@DisplayName("시나리오에따른 인수테스트")
+	class 인수테스트 {
+		@Test
+		@DisplayName("유일한 우승자가 존재")
+		void 다수의자동차_우승자_1명() {
+			try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+			}
+		}
+
+		@Test
+		@DisplayName("다수의 우승자가 존재")
+		void 다수의자동차_우승자_2명() {
+			try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+			}
+		}
+	}
+
+	@Nested
+	@DisplayName("자동차 Domain의 기능 테스트")
+	class 자동차 {
+		@Test
+		@DisplayName("생성오류-생성규칙(5이하) 위배")
+		void 생성오류_이름5자초과() {
+		}
+
+		@Test
+		@DisplayName("생성오류-자동차 0개만 생성")
+		void 생성오류_생성_0개() {
+		}
+
+		@Test
+		@DisplayName("자동차를 1개만 생성")
+		void 생성_1개() {
+		}
+
+		@Test
+		@DisplayName("자동차 2개만 생성")
+		void 생성_2개() {
+		}
+
+		@Test
+		@DisplayName("정지로 1번만 이동")
+		void 이동거리누적_정지() {
+		}
+		
+		@Test
+		@DisplayName("전진로 1번만 이동")
+		void 이동거리누적_전진() {
+		}
+		
+		@Test
+		@DisplayName("연속하여 정지로 이동")
+		void 이동거리누적_정지_정지() {
+		}
+
+		@Test
+		@DisplayName("연속하여 전진으로 이동")
+		void 이동거리누적_전진_전진() {
+		}
+		
+		@Test
+		@DisplayName("정지와 전진을 복합해서 이동")
+		void 이동거리누적_전진_정지_전진() {
+		}
+	}
+
+	@Nested
+    @DisplayName("자동차를 움직이는 방법")
+	class 자동차_움직임_전략 {
+		@Test
+		@DisplayName("앞으로 움직임")
+		void 전진() {
+		}
+
+		@Test
+		@DisplayName("움직이지 않음")
+		void 정지() {
+		}
+	}
 
     @AfterEach
     void tearDown() {
