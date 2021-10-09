6c6f2f77c1dcc291e39220dc9e512448e4c7a8eb
LunChaser <devilking12@naver.com>
2021-10-09 12:20:53 +0900

    refactor(Application, UI): mainApplication의 UI로직 이동, UI Layer 추가
    mainApplication의 UI관련 로직을 UI Layer로 이동

--- src/main/java/racinggame/RacingGame.java
+++ src/main/java/racinggame/RacingGame.java
@@ -1,8 +1,7 @@
 package racinggame;
 
-import nextstep.utils.Console;
-import racinggame.constants.RacingConstants;
 import racinggame.domain.car.RacingCarList;
+import racinggame.ui.RacingGameUi;
 
 public class RacingGame {
 	RacingCarList racingCars;
@@ -12,75 +11,17 @@ public class RacingGame {
 	}
 
 	public void run() {
-		racingCars = getRacingCars();
+		racingCars = RacingGameUi.getRacingCars();
 
-		Integer racingCount = getRacingCount();
+		Integer racingCount = RacingGameUi.getRacingCount();
 
 		for (Integer index = 0; index < racingCount; index++) {
 			racingCars.allMove();
 		}
 
-		printRacingResult(racingCars);
+		RacingGameUi.printRacingResult(racingCars);
 
-		printRacingWinner(getWinners(racingCars));
-	}
-
-	private RacingCarList getRacingCars() {
-		RacingCarList tempRacingCars = new RacingCarList();
-		Integer racingCarCount = 0;
-
-		while (racingCarCount < 1) {
-			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-			tempRacingCars.add(Console.readLine());
-
-			racingCarCount = tempRacingCars.size();
-			printInvalidRacingCarGenerate(racingCarCount);
-		}
-
-		return tempRacingCars;
-	}
-
-	private Integer getRacingCount() {
-		Integer racingCount = 0;
-
-		while (racingCount < 1) {
-			System.out.println("시도할 회수는 몇회인가요?");
-			racingCount = Integer.valueOf(Console.readLine());
-
-			printInvalidRacingCount(racingCount);
-		}
-
-		return racingCount;
-	}
-
-	private void printInvalidRacingCarGenerate(Integer racingCarCount) {
-		if (racingCarCount < 1) {
-			System.out.println("[ERROR] 자동차 생성을 하지 못하였습니다. - 입력하신 자동차 이름을 확인해주세요.");
-		}
-	}
-
-	private void printInvalidRacingCount(Integer racingCount) {
-		if (racingCount < 1) {
-			System.out.println("[ERROR] 시도할 회수는 0이상입니다.");
-		}
-	}
-
-	public void printRacingResult(RacingCarList cars) {
-		System.out.println("실행 결과");
-
-		for (Integer carIndex = 0; carIndex < cars.size(); carIndex++) {
-			System.out.println(cars.getName(carIndex) + " : " + printCarDistance(cars.getCarMoveDistance(carIndex)));
-		}
-	}
-
-	private String printCarDistance(Integer moveDistance) {
-		String carMoveDistance = "";
-
-		for (Integer distance = 0; distance < moveDistance; distance++) {
-			carMoveDistance += RacingConstants.CAR_MOVE_VIEW;
-		}
-
-		return carMoveDistance;
+		RacingGameUi.printRacingWinner(getWinners(racingCars));
 	}
 
 	public String getWinners(RacingCarList cars) {
@@ -101,9 +42,5 @@ public class RacingGame {
 
 	public RacingCarList getWinnerList(Integer maxDistance, RacingCarList racingCars) {
 		return racingCars.findBy(maxDistance);
-	}
-
-	public void printRacingWinner(String winners) {
-		System.out.println("최종 우승자는 " + winners + " 입니다.");
 	}
 }
---
+++ src/main/java/racinggame/ui/RacingGameUi.java
@@ -0,0 +1,85 @@
+package racinggame.ui;
+
+import nextstep.utils.Console;
+import racinggame.constants.RacingConstants;
+import racinggame.domain.car.RacingCarList;
+
+public class RacingGameUi {
+	public static String getCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+		return Console.readLine();
+	}
+
+	public static RacingCarList getRacingCars() {
+		RacingCarList tempRacingCars = new RacingCarList();
+
+		while (tempRacingCars.size() < 1) {
+			tempRacingCars = generateRacingCars();
+		}
+
+		return tempRacingCars;
+	}
+
+	private static RacingCarList generateRacingCars() {
+		RacingCarList tempRacingCars = new RacingCarList();
+
+		if (!tempRacingCars.add(getCarName())) {
+			System.out.println("[ERROR] 자동차 생성을 하지 못하였습니다. - 입력하신 자동차 이름을 확인해주세요.");
+		}
+
+		return tempRacingCars;
+	}
+
+	public static Integer getRacingCount() {
+		Integer racingCount = -1;
+
+		while (isInvalidRacingCount(racingCount)) {
+			System.out.println("시도할 회수는 몇회인가요?");
+
+			racingCount = Integer.valueOf(Console.readLine());
+
+			printInvalidRacingCount(racingCount);
+		}
+
+		return racingCount;
+	}
+
+	private static boolean isInvalidRacingCount(Integer racingCount) {
+		return racingCount < 1;
+	}
+
+	private static void printInvalidRacingCount(Integer racingCount) {
+		if (isInvalidRacingCount(racingCount)) {
+			System.out.println("[ERROR] 시도할 회수는 0이상입니다.");
+		}
+	}
+
+	public static void printRacingResult(RacingCarList cars) {
+		System.out.println("");
+		System.out.println("실행 결과");
+
+		for (Integer carIndex = 0; carIndex < cars.size(); carIndex++) {
+			printMoveDistanceForCar(cars.getName(carIndex), cars.getCarMoveDistance(carIndex));
+		}
+		System.out.println("");
+	}
+
+	private static void printMoveDistanceForCar(String carName, Integer moveDistance) {
+		System.out.println(carName + " : " + printCarMoveDistance(moveDistance));
+	}
+
+	private static String printCarMoveDistance(Integer moveDistance) {
+		String carMoveDistance = "";
+
+		for (Integer distance = 0; distance < moveDistance; distance++) {
+			carMoveDistance += RacingConstants.CAR_MOVE_VIEW;
+		}
+
+		return carMoveDistance;
+	}
+
+	public static void printRacingWinner(String winners) {
+		System.out.println("최종 우승자는 " + winners + " 입니다.");
+	}
+}
